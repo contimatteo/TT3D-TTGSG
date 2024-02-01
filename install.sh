f11f1c7a2d59b77594966a63a44466e0e74a9320
Matteo Conti <contimatteo1505@gmail.com>
2024-02-01 16:43:45 +0100

    WIP

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-pip install -U pip wheel
+pip install -U pip wheel setuptools
 pip install torch torchvision --index-url https://download.pytorch.org/whl/cu118
 pip install open3d
 pip install PyMCubes pytorch-lamb sentencepiece sacremoses plyfile pynrrd
