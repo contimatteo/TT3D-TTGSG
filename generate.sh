d59692b3b32b44f740a89677c8b7074491a792cc
Matteo Conti <contimatteo1505@gmail.com>
2024-02-01 16:55:30 +0100

    WIP

--- generate.sh
+++ generate.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
+
+
 exit 0
 
+
+###
+
+cd generation/checkpoint/color_all_ae_64
+gdown -c 1sc7HRpL3e8oYJXlN3CjLioTRln2GkXai
+unzip -jn code-zz.zip -d ./
+unzip -jn checkpoint.zip
+unzip -n hdf5_test_new.zip -d ../../../
+cd ../../../
+
+
+###
+
+
+cd generation/
 CUDA_VISIBLE_DEVICES=2 python main.py \
   --res64 \
   --sample_dir \
   samples/im_ae_out \
   --start 0 \
   --end 7454
+cd ..
--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ pip install -U pip wheel setuptools
 pip install torch torchvision --index-url https://download.pytorch.org/whl/cu118
 pip install open3d
 pip install PyMCubes pytorch-lamb sentencepiece sacremoses plyfile pynrrd
+pip install h5py tokenizers gdown
