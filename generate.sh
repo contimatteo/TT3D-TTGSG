e03f8ee5e99a0bb645f944c9d7896cd361202c9c
Matteo Conti <contimatteo1505@gmail.com>
2024-02-01 17:56:08 +0100

    WIP

--- generate.sh
+++ generate.sh
@@ -6,12 +6,29 @@ exit 0
 
 ###
 
-cd generation/checkpoint/color_all_ae_64
-gdown -c 1sc7HRpL3e8oYJXlN3CjLioTRln2GkXai
-unzip -jn code-zz.zip -d ./
-unzip -jn checkpoint.zip
-unzip -n hdf5_test_new.zip -d ../../../
-cd ../../../
+rm -rf generation/checkpoint/color_all_ae_64/
+# mkdir -p generation/checkpoint/color_all_ae_64/
+rm -rf hdf5_test_new
+# mkdir hdf5_test_new
+
+# rm -rf /media/data2/mconti/TT3D/checkpoints/TTGSG/generation/
+mkdir -p /media/data2/mconti/TT3D/checkpoints/TTGSG/generation/
+
+gdown -c 1sc7HRpL3e8oYJXlN3CjLioTRln2GkXai -O /media/data2/mconti/TT3D/checkpoints/TTGSG/generation/
+unzip -jn /media/data2/mconti/TT3D/checkpoints/TTGSG/generation/code-zz.zip -d /media/data2/mconti/TT3D/checkpoints/TTGSG/generation/
+
+unzip -n /media/data2/mconti/TT3D/checkpoints/TTGSG/generation/hdf5_test_new.zip -d /media/data2/mconti/TT3D/checkpoints/TTGSG/generation
+ln -s /media/data2/mconti/TT3D/checkpoints/TTGSG/generation/hdf5_test_new ./
+
+unzip -jn /media/data2/mconti/TT3D/checkpoints/TTGSG/generation/checkpoint.zip -d /media/data2/mconti/TT3D/checkpoints/TTGSG/generation/checkpoints/
+# mv /media/data2/mconti/TT3D/checkpoints/TTGSG/generation/checkpoints/ /media/data2/mconti/TT3D/checkpoints/TTGSG/generation/color_all_ae_64/
+# mkdir -p /media/data2/mconti/TT3D/checkpoints/TTGSG/generation/checkpoint/color_all_ae_64/
+# mv /media/data2/mconti/TT3D/checkpoints/TTGSG/generation/color_all_ae_64/ /media/data2/mconti/TT3D/checkpoints/TTGSG/generation/checkpoint/
+# ln -s /media/data2/mconti/TT3D/checkpoints/TTGSG/generation/checkpoint/color_all_ae_64/ generation/checkpoint/
+ln -s /media/data2/mconti/TT3D/checkpoints/TTGSG/generation/checkpoint/color_all_ae_64/res64.model64-199.pth generation/checkpoint/color_all_ae_64/ res64.model64-199.pth
+
+
+
 
 
 ###
